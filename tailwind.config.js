649fdeb0f5a1848196262d3effacd346f754ea87
kratos-te <michaelgordon.kratos@gmail.com>
2023-03-10 23:29:38 +0900

    add discord

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
  content: [
    "./pages/**/*.{js,ts,jsx,tsx}",
    "./components/**/*.{js,ts,jsx,tsx}",
+   "./public/**/*.html",
  ],
  theme: {
    extend: {
@@ -19,5 +20,7 @@ module.exports = {
     }
    },
  },
- plugins: [],
+ plugins: [
+  require("flowbite/plugin")
+],
 }
