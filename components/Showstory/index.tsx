f17614c108976abe12a3fcd6dfa4432393ea9455
kratos-te <michaelgordon.kratos@gmail.com>
2023-03-13 23:34:08 +0900

    fix font-color

--- components/Showstory/index.tsx
+++ components/Showstory/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable react/no-unescaped-entities */
-import { motion } from 'framer-motion'
-import WavyText from '../../global_componenets/WavyText'
+import { motion } from "framer-motion";
+import WavyText from "../../global_componenets/WavyText";
 
 export default function Showstory() {
   return (
@@ -9,50 +9,85 @@ export default function Showstory() {
       <div className="flex text-center font-bold mt-36 lg:text-[65px] mb-24 w-[100%] items-center justify-center md: text-[28px] md:font-[900]">
         <WavyText text="Chapter 1: The Rise of Red Devil" />
       </div>
-      <div className="grid justify-items-center px-12 py-24">
-      <h1 className="text-indigo-600 font-bold text-xl">
-            The city was in chaos. Buildings crumbled to the ground, and people ran for their lives as Red Devil, the notorious villain, wreaked havoc in the streets. He was a formidable foe, with powers beyond imagination. His skin was red as fire, and his eyes glowed with an eerie light. He had the ability to conjure flames at will, and he used them to destroy everything in his path. </h1><br />
-            <h1 className="text-indigo-600 font-bold text-xl">
-            No one knew where Red Devil had come from, or what had driven him to such madness. Some said he was a demon from another realm, summoned by dark magic. Others said he was a victim of a cruel experiment gone wrong. But whatever his origins, there was no denying that he was a danger to society.</h1> <br />
-            <h1 className="text-indigo-600 font-bold text-xl">
-            The police had tried to stop him, but their weapons were useless against his fiery power. The military had been called in, but even they were no match for Red Devil. It seemed as though nothing could stop him. 
-              But then, something unexpected happened. In the midst of his rampage, Red Devil spotted a young girl, cowering in fear behind a burning car. She was just a child, innocent and helpless. For a moment, Red Devil hesitated. He had never felt sympathy for anyone before, but something about the girl touched him. He wondered what had brought her into this chaos, and what would become of her if he continued on his destructive path.
-              </h1> <br />
-            <h1 className="text-indigo-600 font-bold text-xl">
-            That moment of hesitation was all it took for the police to launch a surprise attack. They fired a barrage of bullets at Red Devil, hoping to catch him off guard. It was a risky move, but it paid off. Red Devil was hit, and he fell to the ground, weakened...
-
-              </h1><br />
-              <h1 className="text-indigo-600 font-bold text-xl">
-              The police wasted no time in apprehending him, and they brought him to a maximum-security prison. There, he was kept under constant surveillance, his powers neutralized by special restraints. The city breathed a collective sigh of relief. Red Devil had been defeated, and the threat he posed had been neutralized.
-But the young girl whom Red Devil had spared could not forget what she had seen. She had seen something in Red Devil's eyes that day, something that made her believe that there was more to him than just a mindless monster. She began to research his past, looking for any clues that could explain his behavior.
-
-              </h1><br />
-              <h1 className="text-indigo-600 font-bold text-xl">
-              What she found surprised her. Red Devil had not always been a villain. In fact, he had once been a normal human, just like anyone else. But then something had happened, something that had turned him into the monster he had become. The young girl began to wonder if there was a way to reach him, to bring him back to the side of good.
-
-It was a risky proposition, but the girl was determined. She approached the authorities and asked for permission to speak with Red Devil. At first, they refused, but she persisted, and eventually, they relented.
-
-The meeting between the girl and Red Devil was set up in a secure room, with guards watching from behind a two-way mirror. The girl was nervous, but determined. As Red Devil entered the room, she could feel the heat emanating from his body.
-
-              </h1><br />
-              <h1 className="text-indigo-600 font-bold text-xl">
-              "Who are you?" Red Devil growled.
-
-"I'm just someone who wants to help you," the girl replied.
-
-"Help me? I don't need your help."
-
-                  "But you do," the girl said. "I know that you weren't always like this. Something happened to you, something that turned you into the monster you are now. But I believe that there's still some humanity left in you. I want to help you find it."
-                  Red Devil looked at the girl, studying her. He had never felt anything like this before. It was as though her words were reaching through
-
-
-              </h1>
-              <h1 className="text-indigo-600 font-bold text-xl">
-              
-                </h1>
-          </div>
-          <img src="/bright-bg.png" className="" alt=""/>
-     
+      <div className="grid justify-items-center px-12 py-24 ">
+        <h1 className="text-indigo-600 font-bold text-xl text-gray-800">
+          The city was in chaos. Buildings crumbled to the ground, and people
+          ran for their lives as Red Devil, the notorious villain, wreaked havoc
+          in the streets. He was a formidable foe, with powers beyond
+          imagination. His skin was red as fire, and his eyes glowed with an
+          eerie light. He had the ability to conjure flames at will, and he used
+          them to destroy everything in his path.{" "}
+        </h1>
+        <br />
+        <h1 className="text-indigo-600 font-bold text-xl text-gray-800">
+          No one knew where Red Devil had come from, or what had driven him to
+          such madness. Some said he was a demon from another realm, summoned by
+          dark magic. Others said he was a victim of a cruel experiment gone
+          wrong. But whatever his origins, there was no denying that he was a
+          danger to society.
+        </h1>{" "}
+        <br />
+        <h1 className="text-indigo-600 font-bold text-xl text-gray-800">
+          The police had tried to stop him, but their weapons were useless
+          against his fiery power. The military had been called in, but even
+          they were no match for Red Devil. It seemed as though nothing could
+          stop him. But then, something unexpected happened. In the midst of his
+          rampage, Red Devil spotted a young girl, cowering in fear behind a
+          burning car. She was just a child, innocent and helpless. For a
+          moment, Red Devil hesitated. He had never felt sympathy for anyone
+          before, but something about the girl touched him. He wondered what had
+          brought her into this chaos, and what would become of her if he
+          continued on his destructive path.
+        </h1>{" "}
+        <br />
+        <h1 className="text-indigo-600 font-bold text-xl text-gray-800">
+          That moment of hesitation was all it took for the police to launch a
+          surprise attack. They fired a barrage of bullets at Red Devil, hoping
+          to catch him off guard. It was a risky move, but it paid off. Red
+          Devil was hit, and he fell to the ground, weakened...
+        </h1>
+        <br />
+        <h1 className="text-indigo-600 font-bold text-xl text-gray-800">
+          The police wasted no time in apprehending him, and they brought him to
+          a maximum-security prison. There, he was kept under constant
+          surveillance, his powers neutralized by special restraints. The city
+          breathed a collective sigh of relief. Red Devil had been defeated, and
+          the threat he posed had been neutralized. But the young girl whom Red
+          Devil had spared could not forget what she had seen. She had seen
+          something in Red Devil's eyes that day, something that made her
+          believe that there was more to him than just a mindless monster. She
+          began to research his past, looking for any clues that could explain
+          his behavior.
+        </h1>
+        <br />
+        <h1 className="text-indigo-600 font-bold text-xl text-gray-800">
+          What she found surprised her. Red Devil had not always been a villain.
+          In fact, he had once been a normal human, just like anyone else. But
+          then something had happened, something that had turned him into the
+          monster he had become. The young girl began to wonder if there was a
+          way to reach him, to bring him back to the side of good. It was a
+          risky proposition, but the girl was determined. She approached the
+          authorities and asked for permission to speak with Red Devil. At
+          first, they refused, but she persisted, and eventually, they relented.
+          The meeting between the girl and Red Devil was set up in a secure
+          room, with guards watching from behind a two-way mirror. The girl was
+          nervous, but determined. As Red Devil entered the room, she could feel
+          the heat emanating from his body.
+        </h1>
+        <br />
+        <h1 className="text-indigo-600 font-bold text-xl text-gray-800">
+          "Who are you?" Red Devil growled. "I'm just someone who wants to help
+          you," the girl replied. "Help me? I don't need your help." "But you
+          do," the girl said. "I know that you weren't always like this.
+          Something happened to you, something that turned you into the monster
+          you are now. But I believe that there's still some humanity left in
+          you. I want to help you find it." Red Devil looked at the girl,
+          studying her. He had never felt anything like this before. It was as
+          though her words were reaching through
+        </h1>
+        <h1 className="text-indigo-600 font-bold text-xl"></h1>
+      </div>
+      <img src="/bright-bg.png" className="" alt="" />
     </div>
-  )
+  );
 }
